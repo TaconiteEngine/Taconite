76f083ccac2d393129c64d59ed4e3a1553e22d87
Rust Bytes <rustbytes@proton.me>
2023-07-24 16:00:36 +0100

    Add some code to compose a new pipeline as a struct

---
+++ src/pipeline_composer.rs
@@ -0,0 +1,82 @@
+use std::{fs, sync::Arc};
+use wgpu::{Device, RenderPipeline, SurfaceConfiguration};
+
+use crate::errors::PipelineError;
+
+pub struct PipelineComposer {
+    device: Arc<Device>,
+    config: SurfaceConfiguration,
+}
+
+impl PipelineComposer {
+    pub fn new(device: Arc<Device>, config: SurfaceConfiguration) -> PipelineComposer {
+        PipelineComposer { device, config }
+    }
+
+    pub fn new_pipeline(&self, shader_path: &str) -> Result<RenderPipeline, PipelineError> {
+        let shader_source = fs::read_to_string(shader_path).map_err(|_| PipelineError::BadPath)?;
+
+        let shader = self
+            .device
+            .create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some("Shader"),
+                source: wgpu::ShaderSource::Wgsl(shader_source.into()),
+            });
+
+        let render_pipeline_layout =
+            self.device
+                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipeline Layout"),
+                    bind_group_layouts: &[],
+                    push_constant_ranges: &[],
+                });
+
+        let render_pipeline = self
+            .device
+            .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",
+                    buffers: &[],
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: self.config.format,
+                        blend: Some(wgpu::BlendState {
+                            color: wgpu::BlendComponent::REPLACE,
+                            alpha: wgpu::BlendComponent::REPLACE,
+                        }),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
+                    // or Features::POLYGON_MODE_POINT
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLIP_CONTROL
+                    unclipped_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                // If the pipeline will be used with a multiview render pass, this
+                // indicates how many array layers the attachments will have.
+                multiview: None,
+            });
+
+        Ok(render_pipeline)
+    }
+}
